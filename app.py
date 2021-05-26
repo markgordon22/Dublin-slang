4c5adf754ba022e16486f21189a2de85c70da030
markgordon22 <markgordon97@gmail.com>
2021-05-26 14:13:42 +0000

    configuration to get the flask app communicating with mongo database

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo  = PyMongo(app)
+
 
 @app.route("/")
 def hello():
