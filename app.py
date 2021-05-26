ffcd804fd5e1f3911142802ef78809efe06cd582
markgordon22 <markgordon97@gmail.com>
2021-05-26 11:22:17 +0000

    secret key configuration - to use functions from flask

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-
-
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
