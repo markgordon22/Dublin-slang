34bc0c43432003f29c4790453c30c33fbdd549b1
markgordon22 <markgordon97@gmail.com>
2021-06-01 15:23:48 +0000

    inserted alert functions this will alert user if email was successful

--- static/js/email.js
+++ static/js/email.js
@@ -10,3 +10,15 @@ let params = {
 
     emailjs.send("service_4b99rtc","template_nllrsno",params)
    .then(
+       // Alert sent if email successful
+    function(){
+            alert("Your email has been sent :) we will be in contact within the next 24 hours");
+            location.reload();
+},
+        // Alert not sent if email was unsuccessful
+    function() {
+            alert("Your email was not sent :( please try again!");
+            location.reload();
+        }
+   );
+   
