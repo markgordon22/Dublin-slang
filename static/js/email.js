0f2ec14a44f5820b250b48e2798655907886bf83
markgordon22 <markgordon97@gmail.com>
2021-06-01 15:13:42 +0000

    the variables assigned to get the values from contact form

--- static/js/email.js
+++ static/js/email.js
@@ -1,26 +1,9 @@
 function sendMail() {
-var params = {
+let params = {
         from_name: "Irish Slang",
         first_name: document.getElementById("fname").value,
         last_name: document.getElementById("lname").value,
         from: "markgordon97@gmail.com",
-        to: document.getElementById("email-address").value,
+        to: document.getElementById("emailaddress").value,
         message: document.getElementById("msg").value
     };
-  
-  emailjs.send("service_4b99rtc","template_nllrsno",params)
-   .then(
-        // Alert sent if email successful
-    function(response){
-            alert("Your email has been sent, we will be in touch as soon as possible");
-            location.reload();
-},
-        // Alert not sent if email was unsuccessful
-    function(error) {
-            alert("Your email was not sent, please try again");
-            location.reload();
-        }
-   );
-   
-return false; //prevents page reloading immediately
-}
