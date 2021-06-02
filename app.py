646de1a2e55ad5cbda65278da4e5626c41897811
markgordon22 <markgordon97@gmail.com>
2021-06-02 23:05:57 +0000

    checks if user inserted existing username and password authenticity

--- app.py
+++ app.py
@@ -31,6 +31,27 @@ def glossary():
 def contact():
     return render_template("contact.html")
 
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
+          
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -59,6 +80,14 @@ def register():
 @app.route('/profile')
 def profile():
     return render_template("/profile.html")
+
+
+@app.route("/get_categories")
+def get_categories():
+    return render_template("")
+
+
+
 
 
 if __name__ == "__main__":
