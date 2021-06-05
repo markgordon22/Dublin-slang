1221a27fb8bb218c96e5d9589195ae84dc2d7b2e
markgordon22 <markgordon97@gmail.com>
2021-06-05 00:27:24 +0000

    delete functionality so user can delete word they added

--- app.py
+++ app.py
@@ -147,6 +147,12 @@ def edit_word(word_id):
     return render_template("edit_word.html", word=word, categories=categories)
 
 
+@app.route("/delete_word/<word_id>")
+def delete_word(word_id):
+    mongo.db.words.remove({"_id": ObjectId(word_id)})
+    flash("word Successfully Deleted!")
+    return redirect(url_for("get_words"))
+
 
 
 if __name__ == "__main__":
