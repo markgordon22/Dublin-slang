c9afb03516c9625c8fc5f87ece387b3a32ed3a27
markgordon22 <markgordon97@gmail.com>
2021-06-05 00:17:33 +0000

    edit submission variable formed to edit key values in db

--- app.py
+++ app.py
@@ -131,6 +131,14 @@ def search():
 
 @app.route("/edit_word/<word_id>", methods=["GET", "POST"])
 def edit_word(word_id):
+    if request.method == "POST":
+        edit_submission = {
+            "category_name": request.form.get("category_name"),
+            "word_name": request.form.get("tword_name"),
+            "word_definition": request.form.get("word_definition"),
+            "word_example": request.form.get("word_example"),
+            "created_by": session["user"]
+        }
 
 
 
