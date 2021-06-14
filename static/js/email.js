296f1dc14749339902c88a868f8dc45abb321553
markgordon22 <markgordon97@gmail.com>
2021-06-14 12:23:31 +0000

    swapped modal for alert() as modals look nicer

--- static/js/email.js
+++ static/js/email.js
@@ -8,20 +8,18 @@ function sendMail() {
         to: document.getElementById("emailaddress").value,
         message: document.getElementById("msg").value
     };
-
-    emailjs.send("service_4b99rtc","template_nllrsno",params)
+   
+    emailjs.send("service_4b99rtc","template_nllrsno", params)
    .then(
        // Alert sent if email successful
-    function(response) {
-            alert("Your email has been sent! :) we will be in contact within the next 24 hours");
-            location.reload();
-},
-        // Alert not sent if email was unsuccessful
-    function(error) {
-            alert("Awww Your email was not sent :( please try submitting all fields again!");
-            location.reload();
-        }
-   )
-     return false; //prevents page reloading immediately
+         
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false;  // To block from loading a new page
 }
 
